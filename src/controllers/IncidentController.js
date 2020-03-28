c5dd992ce18b26c419660fb1f1db988737081ad4
Luis Fernando Zaltron <lfzaltron@gmail.com>
2020-03-28 12:40:47 -0300

    check if incident exists on delete route

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -73,6 +73,10 @@ module.exports = {
             .select('ong_id')
             .first();
 
+        if (incident === undefined) {
+            return response.status(404).json({ error: 'Incident not found.' });
+        }
+
         if (incident.ong_id !== ong_id) {
             return response
                 .status(401)
