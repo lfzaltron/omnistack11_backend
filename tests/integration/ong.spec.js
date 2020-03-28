763b16eb3672e468c2bbc486b2c0f10d1141ca39
Luis Fernando Zaltron <lfzaltron@gmail.com>
2020-03-28 12:59:44 -0300

    add more tests

--- tests/integration/ong.spec.js
+++ tests/integration/ong.spec.js
@@ -26,4 +26,74 @@ describe('ONG', () => {
         expect(response.body).toHaveProperty('id');
         expect(response.body.id).toHaveLength(8);
     });
+
+    it('should not create ONG without name', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                email: 'email@nothing.com',
+                whatsapp: '55999999999',
+                city: 'cidade',
+                uf: 'uf'
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.error.status).toBe(400);
+    });
+
+    it('should not create ONG without email', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: 'ong name',
+                whatsapp: '55999999999',
+                city: 'cidade',
+                uf: 'uf'
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.error.status).toBe(400);
+    });
+
+    it('should not create ONG without whatsapp', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: 'ong name',
+                email: 'email@nothing.com',
+                city: 'cidade',
+                uf: 'uf'
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.error.status).toBe(400);
+    });
+
+    it('should not create ONG without city', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: 'ong name',
+                email: 'email@nothing.com',
+                whatsapp: '55999999999',
+                uf: 'uf'
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.error.status).toBe(400);
+    });
+
+    it('should not create ONG without uf', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: 'ong name',
+                email: 'email@nothing.com',
+                whatsapp: '55999999999',
+                city: 'cidade'
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.error.status).toBe(400);
+    });
 });
